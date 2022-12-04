e9b9248260b6ae87d7d2a03f6202f5335151355b
higa <codehiga@gmail.com>
2022-12-04 19:07:44 -0300

    feat: any url

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["www.pixelstalk.net", "a0.muscache.com", "www.macau.rn.leg.br"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
